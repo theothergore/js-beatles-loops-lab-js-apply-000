ee41bc24a654f22c354c611b22b76b4347acf61c
Fly J Gore <jennifer@theothergore.com>
2017-12-19 21:32:13 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,5 @@
+function theBeatlesPlay(){
+  const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+  const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+}
+
