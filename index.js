56597ea4ce3372d49a37166b258b86992bf5cff8
Fly J Gore <jennifer@theothergore.com>
2017-12-19 22:00:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
-function theBeatlesPlay() {
+function theBeatlesPlay(musicians, instruments) {
   var array = [];
   for (let i = 0; i < 4; i++) {
       array.push(`${musicians[0]} plays ${instruments[0]}`);
