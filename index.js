d227efd1bb552125373b0f37c30fdbc007c12695
Fly J Gore <jennifer@theothergore.com>
2017-12-19 21:47:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,11 +2,24 @@ const musicians = ["John", "Paul", "George", "Ringo"];
 
 const instruments = ["guitar", "guitar", "bass", "drums"];
 
-function theBeatlesPlay(musicians, instruments){
-  var array = []
-  for (let i = 0; i < 4; i++){
-    array.push('${musicians[0]} plays ${instruments[0]}');
-  }
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (let i = 0; i < 4; i++) {
+      array.push(`${musicians[0]} plays ${instruments[0]}`);
+    }
   return array;
+}
+
+
+
+
+
+
+// function theBeatlesPlay(musicians, instruments){
+//   var array = []
+//   for (let i = 0; i < 4; i++){
+//     array.push('${musicians[0]} plays ${instruments[0]}');
+//   }
+//   return array;
 
 }
