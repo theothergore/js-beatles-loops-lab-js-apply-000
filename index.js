e7b1dfb2e42cace3528ed6bc4b28c637efdac143
Fly J Gore <jennifer@theothergore.com>
2017-12-19 21:53:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,30 @@ function theBeatlesPlay(musicians, instruments) {
     }
   return array;
 }
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts(facts) {
+  while () {
+    facts[n] + "!!!";
+  }
+  return facts;  
+}
+
+function iLoveTheBeatles(x) {
+  var array = [];
+  do {
+    x = array.push("I love the Beatles!");
+    x++;
+  } while (x < 15); {
+    
+  }
+  return array;
+}
+
+iLoveTheBeatles(12);
